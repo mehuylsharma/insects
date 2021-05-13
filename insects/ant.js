4ae64e255bd3ff259117a3333da44d94ecf196db
Mehul <the8bitmehul@gmail.com>
2021-05-13 11:01:07 +0530

    Ants without legs and curve movement

--- insects/ant.js
+++ insects/ant.js
@@ -1,9 +1,6 @@
 class Ant {
-    constructor(x, y, maxVel = 3, maxForce = 0.15, rad = 20) {
+    constructor(x, y, maxVel = 3, maxForce = 0.15, rad = 9) {
         this.pos = createVector(x, y);
-        //this.frontSeg = createVector(0, 10);
-        this.rW = rad*0.6;
-        this.rH = rad*1.6;
         this.maxVel = maxVel;
         this.maxForce = maxForce;
         this.r = rad;
@@ -69,27 +66,20 @@ class Ant {
 
     display() {
         var theta = this.vel.heading() - PI/2;
-/* 
-        this.frontSeg.x = 10*(theta*(1/Math.PI));
-        this.frontSeg.y = 10 - Math.abs(this.frontSeg.x); */
 
         this.update();
 
         push();
 
-        translate(this.pos.x, this.pos.y);
+        translate(this.pos);
         rotate(theta);
 
-        stroke(255);
-        strokeWeight(1.5);
-        noFill();
+        fill(255);
+        noStroke();
 
-        triangle(-this.rW, -1/3*this.rH, this.rW, -1/3*this.rH, 0, 2/3*this.rH);
-
-        /* 
-        ellipse(this.frontSeg.x, this.frontSeg.y, this.r*1.5, this.r*1.5);
-        ellipse(0, 0, this.r, this.r);
-        ellipse(0, -10, this.r*2, this.r*2); */
+        ellipse(0, 8, this.r, this.r);
+        ellipse(0, 0, this.r/2, this.r/2);
+        ellipse(0, -8, this.r*1.1, this.r*1.1);
 
         pop();
     }
